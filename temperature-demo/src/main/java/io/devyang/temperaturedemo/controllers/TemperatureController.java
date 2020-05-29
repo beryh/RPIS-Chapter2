ec3c45a5bb61840354802b81d2ed6f8d21e1e10f
beryh <didgh9589>
2020-05-29 17:27:56 +0900

    Step 2 Temperature Demo Using RxJava

--- temperature-demo/src/main/java/io/devyang/temperaturedemo/controllers/TemperatureController.java
+++ temperature-demo/src/main/java/io/devyang/temperaturedemo/controllers/TemperatureController.java
@@ -1,47 +1,28 @@
 package io.devyang.temperaturedemo.controllers;
 
-import io.devyang.temperaturedemo.domain.Temperature;
-import org.springframework.context.event.EventListener;
-import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.Async;
+import io.devyang.temperaturedemo.artifect.RxSseEmitter;
+import io.devyang.temperaturedemo.artifect.TemperatureSensor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 @RestController
 public class TemperatureController {
-    private final Set<SseEmitter> clients = new CopyOnWriteArraySet<>();
+    private final TemperatureSensor temperatureSensor;
+
+    public TemperatureController(TemperatureSensor temperatureSensor) {
+        this.temperatureSensor = temperatureSensor;
+    }
 
     @RequestMapping(value = "/temperature-stream", method = RequestMethod.GET)
     public SseEmitter events(HttpServletRequest request) {
-        SseEmitter emitter = new SseEmitter();
-        clients.add(emitter);
+        RxSseEmitter emitter = new RxSseEmitter();
 
-        // Remove emitter from clients on error or disconnect
-        emitter.onTimeout(() -> clients.remove(emitter));
-        emitter.onCompletion(() -> clients.remove(emitter));
+        temperatureSensor.temperatureStream().subscribe(emitter.getSubscriber());
 
         return emitter;
-    }
-
-    @Async
-    @EventListener
-    public void handleMessage(Temperature temperature) {
-        List<SseEmitter> deadEmitters = new ArrayList<>();
-        clients.forEach(emitter -> {
-            try {
-                emitter.send(temperature, MediaType.APPLICATION_JSON);
-            } catch(Exception ignore) {
-                deadEmitters.add(emitter);
-            }
-        });
-        clients.removeAll(deadEmitters);
     }
 }
